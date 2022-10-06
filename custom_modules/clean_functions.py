a01123a343a1d744374e89a9ef12257b209d38e8
Arie Twigt <ariekanarie@twigtd4.nl>
2022-10-06 11:44:04 +0200

    added example for cleaning data frame

---
+++ custom_modules/clean_functions.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+def clean_df(df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Function to clean a pandas DataFrame:
+    * right data types
+    * empty values for float and int columns
+
+    Parameters:
+    * data frame
+
+    Returns:
+    * cleaned data frame
+    
+    '''
+    
+    
+    pass
