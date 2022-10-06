11d3ed03adc08b895ddabadb89d760a92d3622f2
Arie Twigt <ariekanarie@twigtd4.nl>
2022-10-06 11:36:55 +0200

    added exples for converting to Pandas DataFrame

--- custom_modules/import_functions.py
+++ custom_modules/import_functions.py
@@ -1,5 +1,8 @@
+import requests
+import pandas as pd
 
-def import_cars_by_brand(brand:str) -> None:
+
+def import_cars_by_brand(brand:str) -> pd.DataFrame:
     '''
     Returns a list of cars from the RDW API
 
@@ -11,6 +14,19 @@ def import_cars_by_brand(brand:str) -> None:
     
     '''
     
+    # explicit is better than implicit
+    brand_upper = brand.upper()
+
+    # define the endpoint
+    endpoint = f"https://opendata.rdw.nl/resource/m9d7-ebf2.json?merk={brand_upper}"
+
+    # execute the request
+    response = requests.get(endpoint)
+
+    # get the content from the response
+    cars_list = response.json()
     
-    
-    pass
+    # convert the list to a pandas DataFrame
+    cars_df = pd.DataFrame(cars_list)
+
+    return cars_df
